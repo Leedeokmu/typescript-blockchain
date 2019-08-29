4c05e96302161321009bc829caface06f8ba77b3
freeefly <freefly3557@gmail.com>
2019-08-30 00:03:07 +0900

    feat : make typescript interface, class

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,42 @@
-const name = "deokmu",
-    age = 24,
-    gender = "mail"
+class Human {
+    private _name:string;
+    private _age:number;
+    private _gender: string;
+    constructor(name:string, age:number, gender:string){
+        this._name = name;
+        this._age = age;
+        this._gender = gender;
+    }
+    set name(name: string) {
+        this._name = name;
+    }
+    set age(value: number) {
+        this._age = value;
+    }
 
-const sayhi = (name:string, age:number, gender:string): string => {
-    return `hello ${name}, you are ${age}, you are a ${gender}`;
+    set gender(value: string) {
+        this._gender = value;
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    get age(): number {
+        return this._age;
+    }
+
+    get gender(): string {
+        return this._gender;
+    }
 }
 
-sayhi("deokmu", 12, "mail");
+const deokmu = new Human('deokmu', 24, 'mail');
+deokmu.age = 12;
+const sayhi = (person: Human): string => {
+    return `hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
+}
+
+console.log(sayhi(deokmu));
 
 export {}
